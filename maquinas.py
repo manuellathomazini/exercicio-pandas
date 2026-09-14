fea32504cdb6511edc53192090bcfdedd02cb61f
Manuella S. Thomazini <manuellathomzn@gmail.com>
2026-09-14 17:18:39 -0300

    feat: ex.1

--- maquinas.py
+++ maquinas.py
@@ -1,8 +1,14 @@
 import pandas as pd
 
-df = pd.read_excel(r"C:\Users\manut\OneDrive\Desktop\python\dataset_falhas_maquinas.xlsx")
+dataset = r"C:\Users\manut\OneDrive\Desktop\exercicio-pandas\dataset_falhas_maquinas.xlsx"
+df = pd.read_excel(dataset)
 
 #1. Crie um filtro para selecionar apenas as máquinas do tipo M, depois do tipo H, depois do tipo L.
+
+m_filter = df['Tipo'] == 'M'
+h_filter = df['Tipo'] == 'H'
+l_filter = df['Tipo'] == 'L'
+
 #2. Crie um filtro para selecionar apenas as falhas do tipo Power Failure.
 #3. Verifique quais máquinas aparecem quando há apenas falhas do tipo power failure.
 #4. Qual a faixa de valores de temperatura do processo, torque e velocidade de rotação quando há apenas falhas do tipo power failure?
